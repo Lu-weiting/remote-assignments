f56f9b56d81fb96e6869f6e3ef14c300c866afea
Lu-weiting <runeeld23@gmail.com>
2023-10-05 17:17:33 +0800

    remote-week2: update signup api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const user_router = require('./Route/user_router');
 
 app.use(express.json());
-app.use('users',user_router);
+app.use('/users',user_router);
 
 
 app.get('/healthcheck', (req, res) => {
